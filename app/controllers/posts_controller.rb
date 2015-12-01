daac5a638ddcdbce5cd0fa7a18bb411f66ba82bc
hamholla <hamhollett@gmail.com>
2015-12-01 12:12:25 -0600

    3rd commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,28 @@ class PostsController < ApplicationController
 	end
 
 	def create  
-	  @post = Post.create(post_params)
-	  redirect_to posts_path
+	  	@post = Post.create(post_params)
+	  	redirect_to posts_path
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to(post_path(@post))
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private

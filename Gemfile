f322aa11f93df4d4d90cc95b87a66982366f52ac
hamholla <hamhollett@gmail.com>
2015-12-02 09:53:22 -0600

    preparing for devise

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'paperclip', '~> 4.2'
 gem 'simple_form', '~> 3.1.0'  
 gem 'bootstrap-sass', '~> 3.3.5'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
@@ -8,31 +10,42 @@ class PostsController < ApplicationController
 	end
 
 	def create  
-	  	@post = Post.create(post_params)
-	  	redirect_to posts_path
+  		if @post = Post.create(post_params)
+	  		flash[:success] = "Your post has been created"
+		  	redirect_to posts_path
+
+		else
+			flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+			render :new
+		end
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
-		@post.update(post_params)
-		redirect_to(post_path(@post))
+		if @post.update(post_params)
+			flash[:success] = "Post updated."
+			redirect_to(post_path(@post))
+		else
+			flash.now[:alert] = "Update failed. Please check the form."
+			render :edit
+		end
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end
 
 	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 
 	def post_params  
 	  params.require(:post).permit(:image, :caption)

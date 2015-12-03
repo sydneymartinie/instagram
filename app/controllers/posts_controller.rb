9027321c12b95a20563261f8aa7db9bfa089cf9c
hamholla <hamhollett@gmail.com>
2015-12-03 09:31:13 -0600

    tay tay 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,19 +7,20 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		@post = Post.new	
-	end
+	    @post = current_user.posts.build
+	  end
 
-	def create  
-  		if @post = Post.create(post_params)
-	  		flash[:success] = "Your post has been created"
-		  	redirect_to posts_path
+	  def create
+	    @post = current_user.posts.build(post_params)
 
-		else
-			flash.now[:alert] = "Your new post couldn't be created! Please check the form."
-			render :new
-		end
-	end
+	    if @post.save
+	      flash[:success] = "Your post has been created!"
+	      redirect_to posts_path
+	    else
+	      flash[:alert] = "Your new post couldn't be created!  Please check the form."
+	      render :new
+	    end
+	  end
 
 	def show
 	end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
-class Post < ActiveRecord::Base  
+class Post < ActiveRecord::Base
+  validates :user_id, presence: true  
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
